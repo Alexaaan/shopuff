8b3e951060acae19e835b99f2bacce3845aff0ce
Alexan Fontana <alexan.fontana@gmail.com>
2026-01-21 23:01:48 +0100

    fix: Use domains instead of remotePatterns for jnrpuff.fr images

--- next.config.ts
+++ next.config.ts
@@ -10,13 +10,6 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     domains: ['jnrpuff.fr'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'jnrpuff.fr',
-        pathname: '/wp-content/uploads/**',
-      },
-    ],
   },
 
   // Experimental features for performance
